6e4e593205c2be634d26b0baa1312ae7403b5301
Kevin Mathis <kevinmathis08@gmail.com>
2021-07-14 00:54:13 -0400

    Added custom global func for pagination calcs

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,40 @@
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const renderPagination = require("./renderPagination");
+const Nunjucks = require("nunjucks");
 
 module.exports = function (eleventyConfig) {
+  let nunjucksEnvironment = new Nunjucks.Environment(
+    new Nunjucks.FileSystemLoader("src/_includes")
+  );
+
+  nunjucksEnvironment.addGlobal("renderPagination", renderPagination);
+
+  eleventyConfig.setLibrary("njk", nunjucksEnvironment);
+
   // SYNTAX HIGHLIGHTING PLUGIN
-  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    // Change which syntax highlighters are installed
+    templateFormats: ["njk", "md"], // default: "*"
+
+    // init callback lets you customize Prism
+    init: function ({ Prism }) {},
+
+    // Added in 3.0, set to true to always wrap lines in `<span class="highlight-line">`
+    // The default (false) only wraps when line numbers are passed in.
+    alwaysWrapLineHighlights: false,
+
+    // Added in 3.0.2, set to false to opt-out of pre-highlight removal of leading
+    // and trailing whitespace
+    trim: true,
+
+    // Added in 3.0.4, change the separator between lines (you may want "\n")
+    lineSeparator: "<br>",
+  });
 
   // SHORT CODES
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  // eleventyConfig.addShortcode("renders", renderPagination);
 
   // FILTERS
   eleventyConfig.addFilter("postDate", (dateObj) => {
